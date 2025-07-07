fe5291c7dd7eff73a4d7f2dc85cdcc12b0d52bd4
talita <talitamsx@gmail.com>
2025-07-07 09:10:24 -0300

    first commit

---
+++ src/main/java/br/com/alura/literalura/Principal.java
@@ -0,0 +1,9 @@
+package br.com.alura.literalura;
+
+import java.util.Scanner;
+
+public class Principal {
+    public void exibeMenu() {
+        private Scanner leitura = new Scanner(System.in);
+    }
+}
